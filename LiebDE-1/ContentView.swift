b18e64159349dc7fb2584fb6e1147e249b6cf722
hsani24 <110380167+hsani24@users.noreply.github.com>
2023-03-02 15:51:04 +0100

    Initial Commit

---
+++ LiebDE-1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LiebDE-1
+//
+//  Created by Sani I on 02.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiebDE-1/LiebDE_1App.swift
@@ -0,0 +1,17 @@
+//
+//  LiebDE_1App.swift
+//  LiebDE-1
+//
+//  Created by Sani I on 02.03.23.
+//
+
+import SwiftUI
+
+@main
+struct LiebDE_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
